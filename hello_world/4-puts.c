4c59279788aa49f4e36f21c4d5dc4c2425ffab24
Schpser <schps.games@gmai.com>
2025-06-16 11:16:41 +0000

    Preprocessor

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,3 +1,2 @@
-#!/bin/bash
 include <stdio.h>
-gcc int main(void) {puts("Programming is like building a multilingual puzzle")return 0;}
+int main(void) {puts("Programming is like building a multilingual puzzle")return 0;}
