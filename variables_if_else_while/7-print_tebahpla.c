258716b6c2c6a34977c5db91c8a5e9dff83617c1
Schpser <schps.games@gmail.com>
2025-06-17 08:54:15 +0000

    Task 7

--- variables_if_else_while/7-print_tebahpla.c
+++ variables_if_else_while/7-print_tebahpla.c
@@ -1,13 +1,15 @@
-int < sdtio.h >
+#include <stdio.h>
 /**
 * main - Entry point
 * Return: Always 0 (Success)
 */
 int main(void)
 {
-for (char letter = 'z'; letter >= 'a'; letter--)
+char letter;
+for (letter = 'z'; letter >= 'a'; letter--)
 {
 putchar(letter);
 }
 putchar('\n');
+return (0);
 }
