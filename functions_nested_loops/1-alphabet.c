c9a626edc5afce5d1d493c033c78356f6f659e63
Schpser <schps.games@gmail.com>
2025-06-17 17:38:10 +0000

    Write a function that prints the alphabet, in lowercase, followed by a new line.

--- functions_nested_loops/1-alphabet.c
+++ functions_nested_loops/1-alphabet.c
@@ -3,7 +3,8 @@
 * print_alphabet - Prints the lowercase alphabet
 *
 * Description: Uses _putchar to print all lowercase letters from 'a' to 'z'
-* followed by a new line. Respects the constraint of using only two _putchar calls.
+* followed by a new line.
+* Respects the constraint of using only two _putchar calls.
 *
 * Return: void (no return value)
 */
