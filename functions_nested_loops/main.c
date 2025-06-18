16c81f2acae0dd246d8995569d924e8e09d6efea
Schpser <schps.games@gmail.com>
2025-06-18 07:21:10 +0000

    Write a function that prints all natural numbers from n to 98, followed by a new line.

--- functions_nested_loops/main.c
+++ functions_nested_loops/main.c
@@ -1,1 +1,9 @@
-echo -e '#include "main.h"\n\nint main(void)\n{\n    print_alphabet_x10();\n    return (0);\n}' > test_main.c
+#include "main.h"  // Cette ligne doit être ABSOLUMENT présente
+
+int main(void)
+{
+print_to_98(0);
+print_to_98(98);
+print_to_98(-10);
+return (0);
+}
