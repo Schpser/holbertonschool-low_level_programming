3c644144a3658588f87b8aa9438d606578689c62
Schpser <schps.games@gmail.com>
2025-06-21 04:25:30 +0000

    Print a text without printf or puts

---
+++ hello_world/101-quote.c
@@ -0,0 +1,12 @@
+#include <unistd.h>
+#include <string.h>
+/**
+*write function : to print a quote
+*return (1)
+*/
+
+int main ()
+{
+write(2,"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",strlen("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n"));
+	return 1;
+}
