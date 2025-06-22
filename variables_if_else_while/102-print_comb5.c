5d5a2ed81ee16489066d9216b35b5013fa1ae837
Schpser <schps.games@gmail.com>
2025-06-22 14:09:34 +0000

    Print unique double-double digits

--- variables_if_else_while/102-print_comb5.c
+++ variables_if_else_while/102-print_comb5.c
@@ -7,15 +7,17 @@
 int main(void)
 {
 int n1, n2;
+
 for (n1 = 0; n1 < 99; n1++)
 {
-for (n2 = n1; n2 <= 99; n2++)
+for (n2 = n1 + 1; n2 <= 99; n2++)
 {
 putchar('0' + n1 / 10);
 putchar('0' + n1 % 10);
 putchar(' ');
 putchar('0' + n2 / 10);
 putchar('0' + n2 % 10);
+
 if (!(n1 == 98 && n2 == 99))
 {
 putchar(',');
