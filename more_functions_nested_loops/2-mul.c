c7bbd27244b1806a509c20ab81822148e4d333e0
Schpser <schps.games@gmail.com>
2025-06-23 10:48:00 +0000

    2-mul

---
+++ more_functions_nested_loops/2-mul.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * int mul - multiply a*b
+ * @a: Character to multiply
+ * @b: Character to multiply
+ * Return: multiply a * b
+ */
+int mul(int a, int b)
+{
+return (a * b);
+}
