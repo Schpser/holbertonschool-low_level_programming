f874e137f5bdd8a0f0deb8cf31c5c2e7d59b6eb8
Schpser <schps.games@gmail.com>
2025-06-23 12:36:01 +0000

    4-print_most_numbers.c

---
+++ more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - print specific numbers
+ */
+
+void print_most_numbers(void)
+{
+int n;
+for (n = 0; n < 10; n++)
+{
+if (n == 2 || n == 4)
+continue;
+else
+{
+_putchar (n + '0');
+}
+_putchar ('\n');
+}
+}
