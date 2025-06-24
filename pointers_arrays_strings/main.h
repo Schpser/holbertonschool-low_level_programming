06f5ef517ea519cbc18674b1387720801038435a
Schpser <schps.games@gmail.com>
2025-06-24 11:33:36 +0000

    Project 2 - C - Pointers, arrays and strings

---
+++ pointers_arrays_strings/main.h
@@ -0,0 +1,13 @@
+int _atoi(char *s);
+int _strlen(char *s);
+
+char *_strcpy(char *dest, char *src);
+
+void reset_to_98(int *n);
+void swap_int(int *a, int *b);
+void print_array(int *a, int n);
+void puts_half(char *str);
+void puts2(char *str);
+void rev_string(char *s);
+void print_rev(char *s);
+void _puts(char *str);
