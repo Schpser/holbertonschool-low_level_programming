3f2595aa865241327b971e728e85b362d27b84f9
Schpser <schps.games@gmail.com>
2025-06-25 13:48:19 +0000

    Print an array

---
+++ pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * print_array - Print characters in the array
+ * @a: pointer of values
+ * @n: The array : Memory of same type elements
+ */
+
+void print_array(int *a, int n)
+{
+int i;
+for (i = 0; i < n; i++)
+{
+printf("%d", a[i]);
+if (i != n - 1)
+{
+printf("; ");
+}
+}
+printf("\n");
+}
