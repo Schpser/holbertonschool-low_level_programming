eb2ab46f062afb3e1d74684dcdd0b711a529b2cc
Schpser <schps.games@gmail.com>
2025-06-26 10:43:45 +0000

    Last Digit

--- variables_if_else_while/1-last_digit.c
+++ variables_if_else_while/1-last_digit.c
@@ -12,7 +12,7 @@ int n;
 int last_digit;
 srand(time(0));
 n = rand() - RAND_MAX / 2;
-last_digit = n % 10; /* Get the last digit */
+last_digit = n % 10; 
 printf("Last digit of %d is %d and is ", n, last_digit);
 if (last_digit > 5)
 {
