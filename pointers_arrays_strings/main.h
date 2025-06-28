7200527498671b480ddfad172ffd8dc5086b611a
Schpser <schps.games@gmail.com>
2025-06-28 11:01:42 +0000

    Added Project 3 Prototypes

--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -2,9 +2,17 @@
 #define MAIN_H
 int _atoi(char *s);
 int _putchar(char c);
+int _putchar(char c);
 
 char *_strcpy(char *dest, char *src);
+char *_strcat(char *dest, char *src);
+char *_strncat(char *dest, char *src, int n);
+char *_strncpy(char *dest, char *src, int n);
+char *string_toupper(char *);
+char *cap_string(char *);
+char *leet(char *);
 
+void reverse_array(int *a, int n);
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
 void print_array(int *a, int n);
