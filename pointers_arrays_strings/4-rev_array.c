0b81447a340d1ae6b031f37456d2e545aae83d2e
Schpser <schps.games@gmail.com>
2025-06-29 07:13:51 +0000

    Reverse an array

---
+++ pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * reverse_array - Swap element in array to reverse it
+ * @a: an array of integers
+ * @n: the number of elements to swap
+ * Return: nothing
+ */
+
+void reverse_array(int *a, int n)
+{
+int i = 0;
+int temp = a[i];
+int end = n - 1;
+a[i] = a[end];
+a[end] = temp;
+while (i < end)
+{
+temp = a[i];
+a[i] = a[end];
+a[end] = temp;
+i++;
+end--;
+}
+}
