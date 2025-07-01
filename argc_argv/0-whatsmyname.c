7036184198ccee2f0fbdb67359da04c255df04e7
Schpser <schps.games@gmail.com>
2025-07-01 23:44:46 +0200

    Rename a folder that prints his name, without compile it again

---
+++ argc_argv/0-whatsmyname.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - Affiche le nom du programme
+ * @argc: Nombre d'arguments (non utilisé)
+ * @argv: Tableau d'arguments
+ * Return: Toujours 0
+ */
+int main(int argc, char *argv[])
+{
+	(void)argc;
+	printf("%s\n", argv[0]);
+	return (0);
+}
