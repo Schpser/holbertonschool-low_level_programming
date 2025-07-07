64110b593a58a44d201bbb4625b5745c31f73ae6
root <root@DESKTOP-O7ACI9L.localdomain>
2025-07-07 15:47:27 +0200

    Return pointer

---
+++ more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * malloc_checked - allocates memory using malloc
+ * @b: the number of bytes to allocate
+ * Return: a pointer to the allocated memory, or exit with status 98 on failure
+ */
+
+void *malloc_checked(unsigned int b)
+{
+	void *ptr = malloc(b);
+
+	if (ptr == NULL)
+	{
+		exit(98);
+	}
+	else
+	{
+		return (ptr);
+	}
+}
