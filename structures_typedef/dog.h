ebba89d60378c00181b9b176562497c6de8675c1
Schpser <schps.games@gmail.com>
2025-07-09 09:17:12 +0200

    main.h

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -22,6 +22,5 @@ void print_dog(struct dog *d);
 void init_dog(struct dog *d, char *name, float age, char *owner);
 dog_t *new_dog(char *name, float age, char *owner);
 void free_dog(dog_t *d);
-int _putchar(char c);
 
 #endif
--- structures_typedef/main.h
+++ structures_typedef/main.h
@@ -1,12 +1,8 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-/*
- * main.h - Prototypes and integers
- */
+#include "dog.h"
 
 int _putchar(char c);
-
-#include "dog.h"
 
 #endif
