9bcb29de1ccc23b10787e5726c50228154573e4f
Schpser <schps.games@gmail.com>
2025-07-29 01:23:57 +0200

    Correction 2 Add Node

--- singly_linked_lists/2-add_node.c
+++ singly_linked_lists/2-add_node.c
@@ -14,6 +14,10 @@ list_t *add_node(list_t **head, const char *str)
 {
 	list_t *new_node = malloc(sizeof(list_t));
 
+	if  (!new_node)
+	{
+		return (NULL);
+	}
 	if (new_node == NULL)
 	{
 		return (NULL);
