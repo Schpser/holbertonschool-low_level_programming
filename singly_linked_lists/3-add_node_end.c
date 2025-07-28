4c45621ec7a8cba3ae57a685ce6fcde89e3e0159
Schpser <schps.games@gmail.com>
2025-07-29 01:02:52 +0200

    3 Add Node End

---
+++ singly_linked_lists/3-add_node_end.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ * add_node_end - To add a node at the end of the list
+ * @str: The string, needs to be duplicated
+ * @head: The beginning of the list
+ * strdup - To duplicate str
+ * Return: The list with the new nod
+ */
+
+list_t *add_node_end(list_t **head, const char *str)
+{
+	list_t *new_node = malloc(sizeof(list_t));
+
+	if (new_node == NULL)
+	{
+		return (NULL);
+	}
+
+	new_node->str = strdup(str);
+
+	if (new_node->str == NULL)
+	{
+		free(new_node);
+		return (NULL);
+	}
+	new_node->len = strlen(str);
+	new_node->next = NULL;
+
+	if (*head == NULL)
+	{
+		*head = new_node;
+	}
+	else
+	{
+		list_t *last_node = *head;
+
+		while (last_node->next != NULL)
+		{
+			last_node = last_node->next;
+		}
+	last_node->next = new_node;
+	}
+	return (new_node);
+}
