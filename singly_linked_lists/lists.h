6a7a5dcfec6bcc0967023f77254dd074e6334d19
Schpser <schps.games@gmail.com>
2025-07-28 16:35:59 +0200

    Print List

--- singly_linked_lists/lists.h
+++ singly_linked_lists/lists.h
@@ -1,6 +1,21 @@
 #ifndef LISTS_H
 #define LISTS_H
+#include <stddef.h>
 
 size_t print_list(const list_t *h);
+
+
+/**
+* struct list_s - Singly linked list
+* @str: Char stocked in the node
+* @len: Size of str
+* @next: Pointer to the next node of the list
+*/
+typedef struct list_s
+{
+	char str;
+	unsigned int len;
+	struct list_s *next;
+} list_t;
 
 #endif /*LISTS_H*/
