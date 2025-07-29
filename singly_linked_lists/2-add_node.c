88a4dd8379cf1ab0b21d00fff933931091d0477e
Schpser <schps.games@gmail.com>
2025-07-29 10:43:09 +0200

    Add dup_str

--- singly_linked_lists/2-add_node.c
+++ singly_linked_lists/2-add_node.c
@@ -12,25 +12,31 @@
 
 list_t *add_node(list_t **head, const char *str)
 {
-	list_t *new_node = malloc(sizeof(list_t));
+	list_t *new_node;
+	char *dup_str;
+	unsigned int len = 0;
 
-	if (str == NULL)
+	dup_str = strdup(str);
+
+	if (dup_str == NULL)
 	{
 		return (NULL);
 	}
 
-	if  (!new_node)
+	while (str[len])
 	{
+		len++;
+	}
+
+	new_node = malloc(sizeof(list_t));
+
+	if (new_node == NULL)
+	{
+		free(dup_str);
 		return (NULL);
 	}
 
-	new_node->str = strdup(str);
-	if (new_node->str == NULL)
-	{
-		free(new_node);
-		return (NULL);
-	}
-
+	new_node->str = dup_str;
 	new_node->len = strlen(str);
 	new_node->next = *head;
 	*head = new_node;
