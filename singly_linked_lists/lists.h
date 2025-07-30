7a75072e870a6ed9c4dd118e8577363ad729b09f
Schpser <schps.games@gmail.com>
2025-07-30 09:27:38 +0200

    Print First

--- singly_linked_lists/lists.h
+++ singly_linked_lists/lists.h
@@ -18,7 +18,7 @@ typedef struct list_s
 	struct list_s *next;
 } list_t;
 
-
+void print_before_main(void);
 size_t list_len(const list_t *h);
 list_t *add_node(list_t **head, const char *str);
 list_t *add_node_end(list_t **head, const char *str);
