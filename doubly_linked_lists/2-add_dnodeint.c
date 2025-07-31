4fef0bc689d4c666d072df91be383504329cdc2e
Schpser <schps.games@gmail.com>
2025-07-31 14:21:52 +0200

    Change order of reading code

--- doubly_linked_lists/2-add_dnodeint.c
+++ doubly_linked_lists/2-add_dnodeint.c
@@ -12,21 +12,20 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 	dlistint_t *new_node;
 
 	new_node = malloc(sizeof(dlistint_t));
-    if (new_node == NULL)
-    {
+	if (new_node == NULL)
+	{
 		return (NULL);
 	}
 
 	new_node->n = n;
-	new_node->next = *head;
 	new_node->prev = NULL;
-    
-	if (*head == NULL)
-    {
-        new_node->n = NULL;
-        *head = new_node;
-        return (new_node);
-    }
+	new_node->next = *head;
 
+	if (*head != NULL)
+	{
+		(*head)->prev = new_node;
+
+	}
+	*head = new_node;
 	return (new_node);
 }
