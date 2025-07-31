214100fe21d654bc1bd72336872fede14d65be06
Schpser <schps.games@gmail.com>
2025-07-31 14:13:01 +0200

    Sum of nodes in the linked list

---
+++ doubly_linked_lists/6-sum_dlistint.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+
+/**
+ * sum_dlistint - The summ of nodes in the list
+ * @head: Pointer to the head of the list.
+ * Return: The sum of the nodes, 0 if the list's empty
+ */
+
+int sum_dlistint(dlistint_t *head)
+{
+	int sum = 0;
+
+	if (head == NULL)
+		return ('0');
+
+	while (head != NULL)
+	{
+		sum += head->n;
+		head = head->next;
+	}
+
+	return (sum);
+}
